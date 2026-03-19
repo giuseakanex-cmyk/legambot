6229399b64c9569d59a71e56f9cec7a25e3b7f38
Giuse <giuseakanex@gmail.com>
2026-03-19 12:22:45 +0100

    Update bot-autolevelup.js

--- plugins/bot-autolevelup.js
+++ plugins/bot-autolevelup.js
@@ -136,7 +136,7 @@ export async function before(m, { conn }) {
             ctx.font = '30px Arial, sans-serif'
             ctx.fillStyle = '#9f7aea'
             ctx.textAlign = 'center'
-            ctx.fillText('✧ ⋆ ┈ ┈ ⋆ ☾⋆⁺₊✧ varebot ✧₊⁺⋆☽ ⋆ ┈ ┈ ⋆ ✧', width/2, height - 40)
+            ctx.fillText('✧ ⋆ ┈ ┈ ⋆ ☾⋆⁺₊✧ legambot ✧₊⁺⋆☽ ⋆ ┈ ┈ ⋆ ✧', width/2, height - 40)
             
             const buffer = canvas.toBuffer('image/jpeg')
             const caption = `
