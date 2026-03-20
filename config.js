9599dc880c08ab643feb6adef637d891046bc1eb
Giuse <giuseakanex@gmail.com>
2026-03-20 13:05:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ global.errore = '⚠️ *Errore inatteso!* Usa il comando `.segnala <errore>` pe
 global.repobot = 'https://github.com/giuseakanex-cmyk/legambot'
 global.gruppo = 'https://chat.whatsapp.com/bysamakavare'
 global.canale = 'https://whatsapp.com/channel/0029VbB41Sa1Hsq1JhsC1Z1z'
-global.insta = 'https://www.instagram.com/giuse'
+global.insta = 'https://www.instagram.com/tessere____'
 
 /*⭑⭒━━━✦❘🗝️ API KEYS 🌍༺❘✦━━━⭒⭑*/
 
